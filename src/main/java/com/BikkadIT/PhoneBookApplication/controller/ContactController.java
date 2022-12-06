9741997f1c15b8a3bffbeeddbc0b79cfd4434227
santoshbikkad013 <santoshbikkad013@gmail.com>
2022-12-06 20:37:21 +0530

    PHON-05:complete getAll operation

--- src/main/java/com/BikkadIT/PhoneBookApplication/controller/ContactController.java
+++ src/main/java/com/BikkadIT/PhoneBookApplication/controller/ContactController.java
@@ -1,8 +1,44 @@
 package com.BikkadIT.PhoneBookApplication.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.BikkadIT.PhoneBookApplication.entities.Contact;
+import com.BikkadIT.PhoneBookApplication.service.ContactServiceI;
 
 @RestController
 public class ContactController {
+
+	@Autowired
+	private ContactServiceI contactServiceI;
+
+	@PostMapping(value = "/saveContact", consumes = "application/json")
+	public ResponseEntity<String> saveContact(@RequestBody Contact contact) {
+
+		boolean saveContact = contactServiceI.saveContact(contact);
+
+		if (saveContact) {
+			String msg = "Contact Saved Successfully";
+			return new ResponseEntity<String>(msg, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<String>("Contact not Saved Successfully", HttpStatus.CREATED);
+
+		}
+	}
+
+	@GetMapping(value = "getAllContact", produces = "application/json")
+	public ResponseEntity<List<Contact>> getAllContact() {
+
+		List<Contact> allContact = contactServiceI.getAllContact();
+		return new ResponseEntity<List<Contact>>(allContact, HttpStatus.OK);
+
+	}
 
 }
--- src/main/java/com/BikkadIT/PhoneBookApplication/service/ContactServiceImpl.java
+++ src/main/java/com/BikkadIT/PhoneBookApplication/service/ContactServiceImpl.java
@@ -1,8 +1,37 @@
 package com.BikkadIT.PhoneBookApplication.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.BikkadIT.PhoneBookApplication.entities.Contact;
+import com.BikkadIT.PhoneBookApplication.repository.ContactRepository;
+
 @Service
-public class ContactServiceImpl {
+public class ContactServiceImpl implements ContactServiceI {
+
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	@Override
+	public boolean saveContact(Contact contact) {
+		Contact contact2 = contactRepository.save(contact);
+		
+		if(contact2 !=null) {
+			return true;
+		}else {
+			return false;
+		}
+		
+		
+	}
+
+	@Override
+	public List<Contact> getAllContact() {
+		
+		List<Contact> list = contactRepository.findAll();
+		return list;
+	}
 
 }
