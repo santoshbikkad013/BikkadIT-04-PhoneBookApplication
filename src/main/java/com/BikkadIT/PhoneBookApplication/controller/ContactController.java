4d49ccb9da31823d270e765fcdaa43be662abda9
santoshbikkad013 <santoshbikkad013@gmail.com>
2022-12-07 20:22:42 +0530

    PHON-07:Update and delete operation

--- src/main/java/com/BikkadIT/PhoneBookApplication/controller/ContactController.java
+++ src/main/java/com/BikkadIT/PhoneBookApplication/controller/ContactController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +43,45 @@ public class ContactController {
 		return new ResponseEntity<List<Contact>>(allContact, HttpStatus.OK);
 
 	}
+
+	@GetMapping(value = "/getContactById/{contactId}", produces = "application/json")
+	public ResponseEntity<Contact> getContactById(@PathVariable Integer contactId) {
+		Contact contact = contactServiceI.getContactById(contactId);
+		return new ResponseEntity<Contact>(contact, HttpStatus.OK);
+
+	}
+
+	@PutMapping(value = "/updateContact", consumes = "application/json")
+	public ResponseEntity<String> updateContact(@RequestBody Contact contact) {
+
+		boolean saveContact = contactServiceI.updateContact(contact);
+
+		if (saveContact) {
+			String msg = "Contact Updated  Successfully";
+			return new ResponseEntity<String>(msg, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<String>("Contact not Updated Successfully", HttpStatus.CREATED);
+
+		}
+
+	}
+	
+	@DeleteMapping(value="/deleteContact/{contactId}")
+	public ResponseEntity<String> deleteContact(@PathVariable Integer contactId){
+		boolean deleteContact = contactServiceI.deleteContact(contactId);
+		
+		if(deleteContact) {
+			String msg = "Contact Deleted  Successfully";
+			return new ResponseEntity<String>(msg, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<String>("Contact not Deleted Successfully", HttpStatus.CREATED);
+
+		}
+		
+	}
+	
+	
+	
+	
 
 }
--- src/main/java/com/BikkadIT/PhoneBookApplication/service/ContactServiceImpl.java
+++ src/main/java/com/BikkadIT/PhoneBookApplication/service/ContactServiceImpl.java
@@ -1,6 +1,7 @@
 package com.BikkadIT.PhoneBookApplication.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,25 +14,54 @@ public class ContactServiceImpl implements ContactServiceI {
 
 	@Autowired
 	private ContactRepository contactRepository;
-	
+
 	@Override
 	public boolean saveContact(Contact contact) {
 		Contact contact2 = contactRepository.save(contact);
-		
-		if(contact2 !=null) {
+
+		if (contact2 != null) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
-		
-		
+
 	}
 
 	@Override
 	public List<Contact> getAllContact() {
-		
+
 		List<Contact> list = contactRepository.findAll();
 		return list;
+	}
+
+	@Override
+	public Contact getContactById(Integer id) {
+		Contact id2 = contactRepository.findById(id).get();
+		return id2;
+	}
+
+	@Override
+	public boolean updateContact(Contact contact) {
+		Contact contact2 = contactRepository.save(contact);
+
+		if (contact2 != null) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	@Override
+	public boolean deleteContact(Integer id) {
+
+		boolean existsById = contactRepository.existsById(id);
+		if (existsById) {
+			contactRepository.deleteById(id);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
